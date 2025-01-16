a0344b4ce10195b7648bb9211ea7fe3e4675eac0
DC Elliott <davecliveelliott@hotmail.com>
2025-01-16 18:49:49 -0330

    Fixed default output by adding 'argCheck' and improved error handling.

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
 const process = require ("process");
-
 const arguments = process.argv.slice(2);
 let pswdLength = '';
 const alpha = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 let pswd = "";
+let argCheck = false;
 
 for (let i = 0; i < arguments.length; i++) {
     if (arguments[i] === '--length') {
-        console.log(arguments)
         pswdLength = arguments[i+1];
-        console.log(pswdLength)
-
-        if (pswdLength === '') {
-            pswdLength = '8';
+        if (!pswdLength) {
+            pswdLength= 8;
         }
+        argCheck = true;
     }else if (arguments[i] === '--help') {
         console.log("HELP and Instructions")
-        }
-    };
+        argCheck = true;
+    }else if (arguments[i] != '--length' || arguments[i] != '--help') {
+        console.log("This is not a valid command. Enter '--help' for instrunctions.")
+    }
+    }
+    
+if (argCheck === false) {
+    pswdLength= 8;
+}
 
 let pswdLengthNum = parseInt(pswdLength);
-console.log(pswdLengthNum)
 
 for (let i=1; i<=pswdLengthNum; i++) {
     let randAlpha = alpha[Math.floor(Math.random()*26)]
